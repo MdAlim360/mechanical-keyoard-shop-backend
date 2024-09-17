7122281093cff53406eec370618e1e9058a5ed80
Md Alim <MdAlim360>
2024-09-18 01:04:43 +0600

    update code

--- src/app/modules/products/products.interface.ts
+++ src/app/modules/products/products.interface.ts
@@ -1,21 +1,10 @@
-
-export interface TProducts {
-    name: string
-    image?: string
-    description: string
-    price: number
-    category: string
-    tags: string[]
-    variants: Variant[]
-    inventory: Inventory
-}
-
-export interface Variant {
-    type: string
-    value: string
-}
-
-export interface Inventory {
-    quantity: number
-    inStock: boolean
+export type TProducts = {
+    image: string,
+    name: string,
+    brand: string,
+    available_quantity: number,
+    price: number,
+    rating: number,
+    description: string,
+    isDeleted?: boolean,
 }
